55c1b0e07a6a379fb222004c6ede909928b91e5b
Ahmed <ahmedelsharawy117@gmail.com>
2025-07-13 00:29:29 +0300

    feat: init redis client

---
+++ utils/client.ts
@@ -0,0 +1,21 @@
+import { createClient, type RedisClientType } from "redis";
+
+let client: RedisClientType | null = null;
+
+export const initRedisClient = async () => {
+	if (!client) {
+		client = createClient();
+
+		client.on("error", (err) => {
+			console.error(err);
+		});
+
+		client.on("connect", () => {
+			console.log("Redis connected!");
+		});
+
+		await client.connect();
+	}
+
+	return client;
+};
