6fd0a061fabdcf39b85c1b75291bfc222c87ce00
Ahmed <ahmedelsharawy117@gmail.com>
2025-07-13 00:09:11 +0300

    feat: use error handler

--- index.ts
+++ index.ts
@@ -1,11 +1,16 @@
+import type { Request, Response } from "express";
 import express from "express";
+import { errorHandler, notFound } from "./middleware/errorHandler";
 
 const app = express();
 
 const PORT = (process.env.PORT as unknown as number) || 8080;
 
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
 	res.send("Hello, World!");
 });
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`[server]: Listening on port ${PORT}`));
